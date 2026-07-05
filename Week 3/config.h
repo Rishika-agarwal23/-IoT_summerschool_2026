d55274e0b67a4a3327ff0f4011247813dc139e94
Rishika Agarwal <rishika2305agarwal@gmail.com>
2026-07-05 19:30:17 +0530

    Create config.h

---
+++ Week 3/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+String apiKey = "YOUR_OPENWEATHERMAP_API_KEY";
+String city = "Delhi";       // Change to your city
+String countryCode = "IN";
+
+#endif
